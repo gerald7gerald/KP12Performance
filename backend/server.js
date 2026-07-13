26174ca9cfd0a4e372af5abc3eeaa95f8ef77d03
gerald7gerald <geraldcgarcia7@gmail.com>
2026-07-12 18:14:22 -0700

    fix for elites

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '15mb' }));
+app.use(express.urlencoded({ extended: true, limit: '15mb' }));
 app.use(express.static(path.resolve(__dirname, '..', 'frontend')));
 
 const pool = new Pool({
